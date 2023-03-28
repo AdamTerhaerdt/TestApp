b920e32fc375029dd55396386cb9170e4dd13a22
Adam Terhaerdt <adam.terhaerdt1@gmail.com>
2023-03-28 13:00:26 -0400

    Added DishRow.Js and ability to click on restaurants and view menu items

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -3,10 +3,19 @@ import React from 'react'
 import { StarIcon } from 'react-native-heroicons/solid'
 import { MapPinIcon } from 'react-native-heroicons/outline'
 import { urlFor } from '../sanity'
+import { useNavigation } from '@react-navigation/native'
 
 const RestaurantCard = ({id, imgUrl, title, rating, genre, address, short_description, dishes, long, lat}) => {
+  const navigation = useNavigation();
+  
   return (
-    <TouchableOpacity className='bg-white mr-3 shadow'>
+    <TouchableOpacity
+    onPress={() => {
+        navigation.navigate("Restaurant", {
+          id, imgUrl, title, rating, genre, address, short_description, dishes, long, lat,
+        });
+    }}
+    className='bg-white mr-3 shadow'>
       <Image
         className="h-36 w-64 rounded-sm"
         source={{
